a71bfb04c3f6cd1d3213d359bc253a8d210d99d2
Albatorius62 <scania2645@hotmail.fr>
2024-12-06 14:23:32 +0100

    changement js

--- chifoumi.js
+++ chifoumi.js
@@ -1,37 +1,50 @@
-let choixPossible = [
-    { name: "pierre", image: "pierre.jpg" },
-    { name: "feuille", image: "feuille.jpg" },
-    { name: "ciseau", image: "ciseau.jpg" }
-  ];
-let imageGauche = document.getElementById('image-gauche');
-let imageDroite = document.getElementById('image-droite');
+const choixPossible = ["pierre", "feuille", "ciseau"];
+
+const imageGauche = document.getElementById('image-gauche');
+const imageDroite = document.getElementById('image-droite');
 let resultatGauche = document.getElementById('resultat-gauche');
 let resultatDroite = document.getElementById('resultat-droite');
-let boutonRejouer = document.getElementById('boutonRejouer');
 
-function choixOrdinateur (){
-    return  choixPossible[Math.floor(Math.random() * choixPossible.length )];
+function choixAleatoire() {
+  return choixPossible[Math.floor(Math.random() * choixPossible.length)];
 }
 
-function comparerChoix(choixJoueur, choixOrdi) {
-    if (choixJoueur === choixOrdi) {
-      return "Égalité!";
-    }
-    if (
-            ( choixJoueur==="pierre"&& choixOrdi === "ciseau") ||
-            ( choixJoueur==="feuille"&& choixOrdi === "pierre")||
-            ( choixJoueur==="ciseau"&& choixOrdi === "feuille")
-    ) {
-        return "vous avez gagné!";
-    }
-    return "L'ordinateur gagne!";
+imageGauche.addEventListener('click', function () {
+  const choixJoueur1 = choixAleatoire();
+  afficherImageGauche(choixJoueur1);
+  jouer("pierre");
+});
+
+imageDroite.addEventListener('click', function () {
+  const choixJoueur2 = choixAleatoire();
+  afficherImageDroite(choixJoueur2, 'droite');
+  jouer("pierre");
+});
+function afficherImageGauche(choixJoueur) {
+  imageGauche.style.backgroundImage = 'url(images-pfc/' + choixJoueur + '.jpg)';
+}
+function afficherImageDroite(choixJoueur) {
+  imageDroite.style.backgroundImage = 'url(images-pfc/' + choixJoueur + '.jpg)';
 }
 
-function jouer(choixJoueur) {
-    let choixOrdi = choixOrdinateur().name;
-    let resultat = comparerChoix(choixJoueur, choixOrdi);
-    console.log("Choix du joueur: " + choixJoueur);
-    console.log("Choix de l'ordinateur: " + choixOrdi);
-    console.log(resultat);
+function comparerChoix(choixJoueur1, choixJoueur2) {
+  if (choixJoueur1 === choixJoueur2) {
+    return "Égalité!";
+  }
+  if (
+    (choixJoueur1 === "pierre" && choixJoueur2 === "ciseau") ||
+    (choixJoueur1 === "feuille" && choixJoueur2 === "pierre") ||
+    (choixJoueur1 === "ciseau" && choixJoueur2 === "feuille")
+  ) {
+    return "le joueur 1 gagne!";
+  }
+  return "Le joueur 2 gagne!";
+}
+function jouer(choixJoueur1, choixJoueur2) {
+
+  let resultat = comparerChoix(choixJoueur1, choixJoueur2);
+  console.log("Choix du joueur 1: " + choixJoueur1);
+  console.log("Choix du joueur 2: " + choixJoueur2);
+  console.log(resultat);
 }
 jouer("pierre");
